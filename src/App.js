1bce020bdfa0ef9fc9697cb72a990de60924e3b9
UweK17 <uwekremer1971@gmx.de>
2023-01-11 22:35:17 +0100

    player componenten angelegt

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Calculator from './components/Calculator.js';
 function App() {
   return (
     <>
-      <div class="partitionScreen">
-        <div class="topLeft"> 1</div>
-        <div class="bottomLeft"> 2</div>
-        <div class="topMiddle"> 3</div>
-        <div class="middleMiddle">
+      <div className="partitionScreen">
+        <div className="topLeft player1">Player 1
+          <div className="player1score">501</div>
+        </div>
+        <div className="bottomLeft player2">Player 2
+          <div className="player2score">501</div>
+        </div>
+        <div className="topMiddle"> 3</div>
+        <div className="middleMiddle">
           <Calculator />
         </div>
-        <div class="bottomMiddle"> 5</div>
-        <div class="topRight"> 6</div>
-        <div class="bottomRight"> 7</div>
+        <div className="bottomMiddle"> 5</div>
+        <div className="topRight player3">Player 3
+          <div className="player3score">501</div>  
+        </div>
+        <div className="bottomRight player4">Player 4
+          <div className="player4score">501</div>
+        </div>
       </div>
     </>
   );
