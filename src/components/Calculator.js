4e6dc6225f222b9a3a96c93d6c8d4bdec7d3cd35
UweK17 <uwekremer1971@gmx.de>
2023-01-11 20:46:59 +0100

    style nachgebssert Centering

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,19 +7,19 @@ const Calculator = () => {
       <div className='display'>
         Display
       </div>
-      <div class="calculatorLayout">
-        <div class="div1"> 1</div>
-        <div class="div2"> 2</div>
-        <div class="div3"> 3</div>
-        <div class="div4"> 4</div>
-        <div class="div5"> 5</div>
-        <div class="div6"> 6</div>
-        <div class="div7"> 7</div>
-        <div class="div8"> 8</div>
-        <div class="div9"> 9</div>
-        <div class="divClear"> Clear</div>
-        <div class="div0"> 0</div>
-        <div class="divDelete"> Delete</div>
+      <div className="calculatorLayout">
+        <div className="div1"> 1</div>
+        <div className="div2"> 2</div>
+        <div className="div3"> 3</div>
+        <div className="div4"> 4</div>
+        <div className="div5"> 5</div>
+        <div className="div6"> 6</div>
+        <div className="div7"> 7</div>
+        <div className="div8"> 8</div>
+        <div className="div9"> 9</div>
+        <div className="divClear"> Clear</div>
+        <div className="div0"> 0</div>
+        <div className="divDelete"> Delete</div>
       </div>
       <button className="scoreSubmit">Submit</button>
     </>
