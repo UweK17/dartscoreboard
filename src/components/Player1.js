87790eddd5c72434b1c97cbb6a503fc5480a6373
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-12 10:36:27 +0100

    das ergebnis player1Score minus currentScore wird angezeigt, nur an falscher Stelle

--- src/components/Player1.js
+++ src/components/Player1.js
@@ -3,20 +3,24 @@ import React, {useState} from 'react';
 const Player1 = ({currentScore}) => {
   
   const [player1Score, setPlayer1Score] = useState(301);
-  console.log('nach dem usestate'+player1Score);
+  console.log('nach dem usestate '+player1Score);
+  console.log(typeof player1Score);
   console.log('currentscore in player1 '+currentScore);
+  console.log(typeof currentScore);
 
-  const result = () => {
-    return setPlayer1Score(player1Score - currentScore)
-  }
+  const result = parseFloat(player1Score-currentScore);
+  console.log('ergebnis '+result);
+  console.log(typeof result);
 
+  
+
+  
   return (
     <>
       <div>Player1</div>
       <div>Startwert: {player1Score}</div>
-      <div className='player1score'>Neuer Wert: {'Neuer Wert: '+ {result}}
+      <div className='player1score'>Neuer Wert: {'Neuer Wert: '+ +result}
       </div>
-      {result}
     </>
   )
 }
