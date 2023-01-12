38133e7fa22c7d7b952919dc8f2e96928d8a0713
UweK17 <uwekremer1971@gmx.de>
2023-01-12 20:26:27 +0100

    Versuch mit let scheitert am neuen Rendern

---
+++ src/components/Player1 copy.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+
+
+const Player1 = ({currentScore}) => {
+  
+  const [player1Score, setPlayer1Score] = useState(301);
+  
+  console.log('nach dem usestate '+player1Score);
+  console.log(typeof player1Score);
+  console.log('currentscore in player1 '+currentScore);
+  console.log(typeof currentScore);
+
+  const result = parseFloat(player1Score-currentScore);
+  
+
+  console.log('ergebnis '+result);
+  console.log(typeof result);
+
+    
+  return (
+    <>
+      <div>Player1</div>
+      <div>Startwert: {player1Score}</div>
+      <div className='player1score'>Neuer Wert: {result}
+      </div>
+    </>
+  )
+}
+
+export default Player1
